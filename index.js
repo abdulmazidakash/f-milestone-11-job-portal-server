650a42922ea1732a97e73999b848fc14d152bb70
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-19 17:06:33 +0600

    commit

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 //middleware
-app.use(cors());
+app.use(cors({
+	origin: ['http://localhost:5176'],
+	credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -30,6 +36,19 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+	//jwt auth related apis
+	app.post('/jwt', async(req, res)=>{
+		const user = req.body;
+		const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h'});
+		res
+		.cookie('token', token, {
+			httpOnly: true,
+			secure: false, // http://localhost:5176/signIn
+		})
+		
+		.send({ success: true});
+	})
 
 
 
@@ -72,6 +91,9 @@ async function run() {
 	app.get('/job-application', async(req, res) =>{
 		const email = req.query.email;
 		const query = {applicant_email: email};
+
+		console.log('cuk cuk cuk', req.cookies);
+		
 		const result = await jobApplicationCollection.find(query).toArray();
 
 		//fokira way to aggregate data
